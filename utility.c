43af5bff406239350e81e841b71b71bc3df635da
Passifi <Pascal.wigger@gmx.de>
2026-04-08 23:20:11 +0200

    String Splitter prototype

--- utility.c
+++ utility.c
@@ -1,5 +1,25 @@
 #include "utility.h"
+#include <string.h>
 
+char** splitString(char* str, char delimiter) 
+{
+  char** result = NULL;
+  int seperator = 0;
+  size_t totalLength;
+  for(size_t i; str[i]; i++) {
+    if(str[i] == delimiter) {
+      seperator = i;
+    }   
+    totalLength++;
+  }
+  result = (char**)malloc(sizeof(char**)*2);
+  result[0] = (char*)malloc(sizeof(char)*seperator );
+  memcpy( result[0], str, (size_t)seperator); 
+  result[0][seperator-1] = '\0';
+
+  memcpy( result[1], str+seperator+1, (size_t)(totalLength-seperator)); 
+  return result;
+}
 void printbinary(int value) {
    int mask = 0x8000;
   for(int i = 0; i < 16; i++) {
--- utility.h
+++ utility.h
@@ -2,6 +2,9 @@
 #define UTILITY_H
 #include <string.h> 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
 
+char** splitString(char* str, char delimiter);
 void printbinary(int value);
 #endif // !UTILITY_H
